74c8797770b7b3a21543d8f9daca7794260c409b
LUIS MARIO <luismario@gmail.com>
2018-08-02 11:35:42 -0500

    mi primer git commit

---
+++ prueba.py
@@ -0,0 +1,18 @@
+#Lectura de Datos Flotantes
+a=float(input("ingresa valor de A:"))
+b=float(input("ingresa valor de B:"))
+
+#Operaciones
+c=a+b
+c1=a-b
+c2=a*b
+c3=a/b
+
+#Impresion de resultado5
+print("RESULTADO")
+print"suma:",c
+print"resta:",c1
+print"multiplicacion:",c2
+print"division:",c3
+print"\n"
+
